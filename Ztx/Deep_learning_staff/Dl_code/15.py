0cc7181bf6792ff6cea3d0784a977b967ae0e9ed
nbStudio021222 <116435533+Tqzxz@users.noreply.github.com>
2025-07-31 14:40:08 +0800

    Create 15.py

---
+++ Ztx/Deep_learning_staff/Dl_code/15.py
@@ -0,0 +1,37 @@
+
+
+'''
+
+20 days object:
+
+    1. FlowerBook Computer Vision Parts( CNN, Object Detection, Bouding Box, R-CNN, semantic segmentation, FCN, ECBSR)   -- 4h   *  20   == 80h
+    2. Math( 1. Linear algebra, Calculus, )                                                                              -- 3h   *  20   == 60h
+    3. Projects-: (1)  Construct Body  (2) User Application with Wifi connected with esp32                               -- 3h   *  20   == 60h
+
+
+Record：
+----------| FlowerBook |-----| Math |----| Project |
+Day1：          N               N             N
+Day2：          N               N             N
+Day3：          N               N             N
+Day4：          N               N             N
+Day5：          N               N             N
+Day6：          N               N             N 
+Day7：          N               N             N 
+Day8：          N               N             N 
+Day9：          N               N             N 
+Day10：         N               N             N 
+Day11：         N               N             N  
+Day12：         N               N             N  
+Day13：         N               N             N  
+Day14：         N               N             N 
+Day15：         N               N             N  
+Day16：         N               N             N  
+Day17：         N               N             N  
+Day18：         N               N             N  
+Day19：         N               N             N  
+Day20：         N               N             N  
+
+-----------------------------------------------------
+total:         0h              0h             0h
+'''
